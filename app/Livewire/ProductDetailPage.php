110a8320578b3a6da4a22e3044fb3e625b96e843
Skuddin <sheikh@nextgenitltd.com>
2025-11-05 22:27:42 +0600

    dynamic product page

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Product Details | Nafisa Mart')]
 class ProductDetailPage extends Component
 {
+    public $slug;
+
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.product-detail-page');
+        $product = Product::where('slug', $this->slug)->firstOrFail();
+
+        return view('livewire.product-detail-page',[
+            'product' => $product
+        ]);
     }
 }
